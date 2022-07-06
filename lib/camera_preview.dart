494de134257f0a31ba29f2398c0d70f3e656f25b
jon_s <tw-jon_s@towa-gifu.co.jp>
2022-07-06 18:33:23 +0900

    プレビュー画面に枠線追加

--- lib/camera_preview.dart
+++ lib/camera_preview.dart
@@ -13,6 +13,9 @@ class _CameraPreviewPageState extends State<CameraPreviewPage> {
   CameraController? _controller;
   int _frameCount = 0;
 
+  final Size _scanSize = const Size(320, 200);
+  late Rect _currentScanRegion;
+
   @override
   void initState() {
     super.initState();
@@ -20,9 +23,12 @@ class _CameraPreviewPageState extends State<CameraPreviewPage> {
   }
 
   void _initCamera() async {
+    final camera = (await availableCameras())[0];
     final controller = CameraController(
-      (await availableCameras())[0],
-      ResolutionPreset.low,
+      camera,
+      ResolutionPreset.max,
+      enableAudio: false,
+      imageFormatGroup: ImageFormatGroup.jpeg,
     );
 
     try {
@@ -65,14 +71,63 @@ class _CameraPreviewPageState extends State<CameraPreviewPage> {
     if (_controller == null || !_controller!.value.isInitialized) {
       return Container();
     }
+    final previewSize = _controller!.value.previewSize!;
+    final screenSize = MediaQuery.of(context).size;
+    debugPrint(
+        "previewSize: $previewSize (${previewSize.aspectRatio}) screenSize: $screenSize  (${screenSize.aspectRatio})");
+
+    final scale = (screenSize.aspectRatio >= 1)
+        ? screenSize.width / previewSize.width
+        : screenSize.height / previewSize.height;
+
+    final scaledPreviewSize =
+        Size(previewSize.width * scale, previewSize.height * scale);
+    debugPrint(
+        "scale: $scale scaledPreviewSize: $scaledPreviewSize (${scaledPreviewSize.aspectRatio})");
+
+    final scanRegion = Rect.fromCenter(
+      center: previewSize.center(Offset.zero),
+      width: _scanSize.width / scale,
+      height: _scanSize.height / scale,
+    );
+    _currentScanRegion = Rect.fromLTRB(
+        scanRegion.left.floorToDouble(),
+        scanRegion.top.floorToDouble(),
+        scanRegion.right.ceilToDouble(),
+        scanRegion.bottom.ceilToDouble());
+
+    debugPrint("scanRegion: $scanRegion ($_currentScanRegion)");
+
     return WillPopScope(
       onWillPop: () async {
         await _controller?.stopImageStream();
         Navigator.pop(context, _frameCount.toString());
         return false;
       },
-      child: MaterialApp(
-        home: CameraPreview(_controller!),
+      child: Scaffold(
+        body: SizedBox(
+          height: screenSize.height,
+          width: screenSize.width,
+          child: Stack(
+            alignment: Alignment.center,
+            children: [
+              SizedBox(
+                height: scaledPreviewSize.height,
+                width: scaledPreviewSize.width,
+                child: CameraPreview(_controller!),
+              ),
+              Container(
+                width: _scanSize.width,
+                height: _scanSize.height,
+                decoration: BoxDecoration(
+                    border: Border.all(
+                  color: Colors.red,
+                  width: 2,
+                )),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
